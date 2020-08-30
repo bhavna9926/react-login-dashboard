29a7acc271f8236b47c439437866da0bf1e627f8
bhavna9926 <sharmabhavna703@gmail.com>
2020-08-30 14:12:38 +0530

    Updates

---
+++ precache-manifest.0994d437aede62b988a0b136e9d070a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9674a71e9d841b1a2e5b99c8af4d7d",
+    "url": "/react-login-dashboard/index.html"
+  },
+  {
+    "revision": "a446793ecc5c6c9e6ddc",
+    "url": "/react-login-dashboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3c4c904ee6f88d59c2b",
+    "url": "/react-login-dashboard/static/js/2.09a9772d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-login-dashboard/static/js/2.09a9772d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a446793ecc5c6c9e6ddc",
+    "url": "/react-login-dashboard/static/js/main.83006a65.chunk.js"
+  },
+  {
+    "revision": "87c34c9c7c57e2c63808",
+    "url": "/react-login-dashboard/static/js/runtime-main.747e4b34.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-login-dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
